1db5c3a703abbb553f70f80d6a6139cb25120f29
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2018-11-09 10:47:15 +0100

    Add SharedPreferences

--- app/src/main/java/com/lescour/ben/moodtracker/Historic.java
+++ app/src/main/java/com/lescour/ben/moodtracker/Historic.java
@@ -2,16 +2,49 @@ package com.lescour.ben.moodtracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by benja on 01/11/2018.
  */
-public class Historic extends AppCompatActivity {
+public class Historic extends MainActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historic);
 
+        LinearLayout mHistoricLayout = (LinearLayout) findViewById(R.id.historicLayout);
+        int customWidth = mHistoricLayout.getWidth() / 5;
+
+        TextView mHier = (TextView) findViewById(R.id.hier);
+        mHier.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mHier.getLayoutParams().width = customWidth * (clrHier +1);
+
+        TextView mAvHier = (TextView) findViewById(R.id.avHier);
+        mAvHier.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mAvHier.setWidth(customWidth);
+
+        TextView mThreeDaysAgo = (TextView) findViewById(R.id.threeDaysAgo);
+        mThreeDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mThreeDaysAgo.setWidth(customWidth);
+
+        TextView mFourDaysAgo = (TextView) findViewById(R.id.fourDaysAgo);
+        mFourDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mFourDaysAgo.setWidth(customWidth);
+
+        TextView mFiveDaysAgo = (TextView) findViewById(R.id.fiveDaysAgo);
+        mFiveDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mFiveDaysAgo.setWidth(customWidth);
+
+        TextView mSixDaysAgo = (TextView) findViewById(R.id.sixDaysAgo);
+        mSixDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mSixDaysAgo.setWidth(customWidth);
+
+        TextView mOneWeekAgo = (TextView) findViewById(R.id.oneWeekAgo);
+        mOneWeekAgo.setBackgroundColor(getResources().getColor(lst_colors[clrHier]));
+        mOneWeekAgo.setWidth(customWidth);
     }
+
 }
--- app/src/main/java/com/lescour/ben/moodtracker/MainActivity.java
+++ app/src/main/java/com/lescour/ben/moodtracker/MainActivity.java
@@ -2,18 +2,19 @@ package com.lescour.ben.moodtracker;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences mSharedPreferences;
     private int [] lst_images = {
             R.mipmap.smiley_sad,
             R.mipmap.smiley_disappointed,
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             R.mipmap.smiley_super_happy
     };
     private int imgPosition = 3;
-    private int [] lst_colors = {
+    int [] lst_colors = {
             R.color.faded_red,
             R.color.warm_grey,
             R.color.cornflower_blue_65,
@@ -30,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
             R.color.banana_yellow,
     };
     private int clrPosition = 3;
+    private final String PREF_KEY_COLOR = "PREF_KEY_COLOR";
+    public int clrHier;
     private float initialY;  //Position where you down your finger on the screen.
+    private Dialog addComment;
+    private EditText commentInput;
+    private String comment;
 
     /**
      * Start the application with happy mood, his background color appropriate, historic button and
@@ -41,11 +47,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getColor() != getResources().getColor(R.color.colorAccent)) {
+            clrHier = getColor();
+        }
         // Start with the happy mood.
         FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frame_layout);
         ImageView imgSwipe = (ImageView) findViewById(R.id.imgSwipe);
         imgSwipe.setImageResource(lst_images[imgPosition]);
         frameLayout.setBackgroundColor(getResources().getColor(lst_colors[clrPosition]));
+        saveColor(clrPosition);
 
         ImageButton icNoteAdd = (ImageButton) findViewById(R.id.icNoteAdd);
         ImageButton icHistory = (ImageButton) findViewById(R.id.icHistory);
@@ -67,19 +78,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addYourComment() {
-        final Dialog addComment = new Dialog(MainActivity.this);
-        //addComment.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        addComment = new Dialog(MainActivity.this);
         addComment.setContentView(R.layout.ic_add_comment);
-
+        commentInput = (EditText) findViewById(R.id.comment_input);
         Button mAdd = (Button) addComment.findViewById(R.id.add);
         mAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO penser à sauvegarder le commentaire
+                //comment = commentInput.getText().toString();
                 addComment.cancel();
             }
         });
-
         addComment.show();
     }
 
@@ -107,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
                         imgPosition++;
                         frameLayout.setBackgroundColor(getResources().getColor(lst_colors[clrPosition]));
                         imgSwipe.setImageResource(lst_images[imgPosition]);
+                        saveColor(clrPosition);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         clrPosition = 4;
                         imgPosition = 4;
@@ -119,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
                         imgPosition--;
                         frameLayout.setBackgroundColor(getResources().getColor(lst_colors[clrPosition]));
                         imgSwipe.setImageResource(lst_images[imgPosition]);
+                        saveColor(clrPosition);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         clrPosition = 0;
                         imgPosition = 0;
@@ -129,5 +141,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onTouchEvent(motionEvent);
     }
 
+    private void saveColor(int clrPosition) {
+        // TODO essayer de foutre SharedPreferences en valeur (tout en haut)
+        mSharedPreferences = getSharedPreferences("backgroundColor", MODE_PRIVATE);
+        mSharedPreferences.edit().putInt(PREF_KEY_COLOR, clrPosition).apply();
+    }
+
+    private int getColor() {
+        // TODO essayer de foutre SharedPreferences en valeur (tout en haut)
+        mSharedPreferences = getSharedPreferences("backgroundColor", MODE_PRIVATE);
+        clrHier = mSharedPreferences.getInt(PREF_KEY_COLOR, getResources().getColor(R.color.colorAccent));
+        return clrHier;
+    }
 
 }
