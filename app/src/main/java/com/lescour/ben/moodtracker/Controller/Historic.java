5946fe693f08beef9abaf9e02275b2879b3ce559
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2018-11-15 14:49:55 +0100

    Measure the screen dimension and set the correct length for every textView

--- app/src/main/java/com/lescour/ben/moodtracker/Controller/Historic.java
+++ app/src/main/java/com/lescour/ben/moodtracker/Controller/Historic.java
@@ -3,9 +3,13 @@ package com.lescour.ben.moodtracker.Controller;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.lescour.ben.moodtracker.Model.Mood;
@@ -22,11 +26,11 @@ import static java.lang.System.out;
  * Created by benja on 01/11/2018.
  */
 public class Historic extends AppCompatActivity {
-    private SharedPreferences mSharedPreferences;
     private Calendar calendar;
     private SimpleDateFormat format;
     private Mood mMood;
     private String currentDay;
+    private int customWidth;
     private int [] lst_colors = {
             R.color.faded_red,
             R.color.warm_grey,
@@ -40,15 +44,19 @@ public class Historic extends AppCompatActivity {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.historic);
 
-       LinearLayout mHistoricLayout = (LinearLayout) findViewById(R.id.historicLayout);
-       int customWidth = mHistoricLayout.getWidth() / 5;
-
        // The current date
        calendar = new GregorianCalendar();
        format = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
        format.setCalendar(calendar);
        currentDay = format.format(calendar.getTime());
        out.println(currentDay); // TODO Supprimer cette ligne plus tard
+
+
+       LinearLayout mHistoricLayout = (LinearLayout) findViewById(R.id.historicLayout);
+
+       DisplayMetrics displaymetrics = new DisplayMetrics();
+       getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+       customWidth = displaymetrics.widthPixels / 5;
 
        yesterday();
        twoDaysAgo();
@@ -67,7 +75,7 @@ public class Historic extends AppCompatActivity {
     }
 
     private void getADeserializeMood(String currentDay) {
-        mSharedPreferences = getSharedPreferences("myMood", MODE_PRIVATE);
+        SharedPreferences mSharedPreferences = getSharedPreferences("myMood", MODE_PRIVATE);
         String aMood = mSharedPreferences.getString(currentDay, "{'lstPosition':3}");
         Gson gson = new Gson();
         mMood = gson.fromJson(aMood, Mood.class);
@@ -75,57 +83,156 @@ public class Historic extends AppCompatActivity {
 
     private void yesterday() {
         TextView mYesterday = (TextView) findViewById(R.id.yesterdayText);
+        ImageButton bYesterday = (ImageButton) findViewById(R.id.yesterdayButton);
+        bYesterday.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mYesterday.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mYesterday.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
+        if (mMood.getComment() != null ) {
+            out.println(mMood.getComment()); // TODO EFFACE CETTE LIGNE
+            final String comment = mMood.getComment();
+            bYesterday.setVisibility(View.VISIBLE);
+            bYesterday.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fYesterday = (FrameLayout) findViewById(R.id.yesterdayFrame);
+        fYesterday.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 
     private void twoDaysAgo() {
         TextView mTwoDaysAgo = (TextView) findViewById(R.id.twoDaysAgoText);
+        ImageButton bTwoDaysAgo = (ImageButton) findViewById(R.id.twoDaysAgoButton);
+        bTwoDaysAgo.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mTwoDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mTwoDaysAgo.setWidth(customWidth);
+        if (mMood.getComment() != null ) {
+            final String comment = mMood.getComment();
+            bTwoDaysAgo.setVisibility(View.VISIBLE);
+            bTwoDaysAgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fTwoDaysAgo = (FrameLayout) findViewById(R.id.twoDaysAgoFrame);
+        fTwoDaysAgo.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 
     private void threeDaysAgo() {
         TextView mThreeDaysAgo = (TextView) findViewById(R.id.threeDaysAgoText);
+        ImageButton bThreeDaysAgo = (ImageButton) findViewById(R.id.threeDaysAgoButton);
+        bThreeDaysAgo.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mThreeDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mThreeDaysAgo.setWidth(customWidth);
+        if (mMood.getComment() != null ) {
+            final String comment = mMood.getComment();
+            bThreeDaysAgo.setVisibility(View.VISIBLE);
+            bThreeDaysAgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fThreeDaysAgo = (FrameLayout) findViewById(R.id.threeDaysAgoFrame);
+        fThreeDaysAgo.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 
     private void fourDaysAgo() {
         TextView mFourDaysAgo = (TextView) findViewById(R.id.fourDaysAgoText);
+        ImageButton bFourDaysAgo = (ImageButton) findViewById(R.id.fourDaysAgoButton);
+        bFourDaysAgo.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mFourDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mFourDaysAgo.setWidth(customWidth);
+        if (mMood.getComment() != null ) {
+            final String comment = mMood.getComment();
+            bFourDaysAgo.setVisibility(View.VISIBLE);
+            bFourDaysAgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fFourDaysAgo = (FrameLayout) findViewById(R.id.fourDaysAgoFrame);
+        fFourDaysAgo.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 
     private void fiveDaysAgo() {
         TextView mFiveDaysAgo = (TextView) findViewById(R.id.fiveDaysAgoText);
+        ImageButton bFiveDaysAgo = (ImageButton) findViewById(R.id.fiveDaysAgoButton);
+        bFiveDaysAgo.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mFiveDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mFiveDaysAgo.setWidth(customWidth);
+        if (mMood.getComment() != null ) {
+            final String comment = mMood.getComment();
+            bFiveDaysAgo.setVisibility(View.VISIBLE);
+            bFiveDaysAgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fFiveDaysAgo = (FrameLayout) findViewById(R.id.fiveDaysAgoFrame);
+        fFiveDaysAgo.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 
     private void sixDaysAgo() {
         TextView mSixDaysAgo = (TextView) findViewById(R.id.sixDaysAgoText);
+        ImageButton bSixDaysAgo = (ImageButton) findViewById(R.id.sixDaysAgoButton);
+        bSixDaysAgo.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mSixDaysAgo.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mSixDaysAgo.setWidth(customWidth);
+        if (mMood.getComment() != null ) {
+            final String comment = mMood.getComment();
+            bSixDaysAgo.setVisibility(View.VISIBLE);
+            bSixDaysAgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fSixDaysAgo = (FrameLayout) findViewById(R.id.sixDaysAgoFrame);
+        fSixDaysAgo.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 
     private void oneWeekAgo() {
         TextView mOneWeekAgo = (TextView) findViewById(R.id.oneWeekAgoText);
+        ImageButton bOneWeekAgo = (ImageButton) findViewById(R.id.oneWeekAgoButton);
+        bOneWeekAgo.setVisibility(View.GONE);
         decreaseTheDay();
         getADeserializeMood(currentDay);
         mOneWeekAgo.setBackgroundColor(getResources().getColor(lst_colors[mMood.getLstPosition()]));
-        //mOneWeekAgo.setWidth(customWidth);*/
+        if (mMood.getComment() != null ) {
+            final String comment = mMood.getComment();
+            bOneWeekAgo.setVisibility(View.VISIBLE);
+            bOneWeekAgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Historic.this,comment,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        FrameLayout fOneWeekAgo = (FrameLayout) findViewById(R.id.oneWeekAgoFrame);
+        fOneWeekAgo.getLayoutParams().width = customWidth * (mMood.getLstPosition() +1);
     }
 }
